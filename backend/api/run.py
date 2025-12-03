9fa8823c32f91e9e6d36aae04563a2900f93d969
taehyunan-99 <withblua@gmail.com>
2025-12-03 11:24:31 +0900

    2025-12-03 ATH: update backend

--- backend/api/run.py
+++ backend/api/run.py
@@ -6,7 +6,7 @@ app = create_app()
 if __name__=="__main__":
     reset_relay()
     app.run(host="0.0.0.0",
-            port=5001,
+            port=5000,
             debug=True,
             use_reloader=False,
             threaded = False)
