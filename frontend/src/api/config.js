e3d215e756e7b896a48e234f23e3acb06716a9d5
taehyunan-99 <withblua@gmail.com>
2025-12-09 11:24:01 +0900

    2025-12-09 ATH: update frontend

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,2 +1,2 @@
 // 백엔드 서버 주소
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_SERVER || 'https://inconsecutively-eyelike-ellamae.ngrok-free.dev';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_SERVER;
